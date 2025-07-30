28b4f5d110e4a85f6c446306ed7970eb784e829c
Mahmuda Akhter <mahmudaakhter2468@gmail.com>
2025-07-30 17:43:09 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
 
+// import "./App.css";
+// import About from "./components/Pages/About";
+// import Error from "./components/Pages/Error";
+// import Shop from "./components/Pages/Shop";
+// import Home from "./components/Pages/home";
+// import Rootlayout from "./components/layouts/Rootlayout";
+// import { Routes, Route} from "react-router-dom";
+
+// function App() {
+//   return (
+//     <>
+    
+//       <Routes>
+//         <Route path="/" element={<Rootlayout/>}>
+//         <Route index element={<Home />}/>
+//         <Route path="about" element={<About/>}/>
+//         <Route path="shop" element={<Shop/>}/>
+//         <Route path="*" element={<Error/>}/>
+
+//         </Route>
+//       </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
 import "./App.css";
 import About from "./components/Pages/About";
 import Error from "./components/Pages/Error";
 import Shop from "./components/Pages/Shop";
 import Home from "./components/Pages/home";
 import Rootlayout from "./components/layouts/Rootlayout";
-import { Routes, Route} from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-    
-      <Routes>
-        <Route path="/" element={<Rootlayout/>}>
-        <Route index element={<Home />}/>
-        <Route path="about" element={<About/>}/>
-        <Route path="shop" element={<Shop/>}/>
-        <Route path="*" element={<Error/>}/>
+return (
 
-        </Route>
-      </Routes>
-    </>
-  );
+<Routes>
+<Route path="/" element={<Rootlayout />}>
+<Route index element={<Home />} />
+<Route path="about" element={<About />} />
+<Route path="shop" element={<Shop />} />
+<Route path="*" element={<Error />} />
+</Route>
+</Routes>
+
+);
 }
 
-export default App;
+export default App
